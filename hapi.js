9e9bfd322bb9b2a601f42681b2e46fd89d5e87c1
Song Wei <song.wei@turner.com>
2015-10-12 21:03:48 +0000

    getting config data through http

--- hapi.js
+++ hapi.js
@@ -1,5 +1,7 @@
 var alexa = require('alexa-app');
 var app = new alexa.app('sample');
+var Q = require('q')
+var _ = require('lodash');
 
 app.intent('number', {
 		"slots": {
@@ -28,15 +30,41 @@ var bodyParser = require('body-parser');
 // 	extended: true
 // })); // for parsing application/x-www-form-urlencoded
 
+var request = require('sync-request');
+
+function getText(url) {
+	var resp = request('GET', url);
+	var text = resp.getBody('utf8')
+	return text
+}
+
+var urls = [
+	'https://demo-project-swei-turner.c9.io/node.js/alexa-skill-mock/test/sample_data/intent.json',
+	'https://demo-project-swei-turner.c9.io/node.js/alexa-skill-mock/test/sample_data/slot.json',
+	'https://demo-project-swei-turner.c9.io/node.js/alexa-skill-mock/test/sample_data/utterance.txt',
+	'https://demo-project-swei-turner.c9.io/node.js/alexa-skill-mock/test/sample_data/response.txt'
+]
+
 // Add the route
 server.route({
 	method: 'POST',
 	path: '/sample',
 	handler: function(req, res) {
-		app.request(req.payload) // connect hapi to alexa-app
-			.then(function(response) { // alexa-app returns a promise with the response
-				res(response); // stream it to hapi' output
-			});
+		var operations = _.map(urls, getText);
+		Q.all(operations).then(function(texts) {
+			var intent = JSON.parse(texts[0])
+			var slot = JSON.parse(texts[1])
+			var utterances = texts[2]
+			var responses = texts[3]
+			console.log(intent)
+			console.log(slot)
+			console.log(utterances)
+			console.log(responses)
+			app.request(req.payload) // connect hapi to alexa-app
+				.then(function(response) { // alexa-app returns a promise with the response
+					res(response); // stream it to hapi' output
+				});
+		});
 	}
 });
 
